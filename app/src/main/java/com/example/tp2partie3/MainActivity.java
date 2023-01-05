fe7d20de1732161b0c8ebff5793c4857105cd70c
AnasFajoui <FAJOUI.ANAS@student.emi.ac.ma>
2023-01-05 20:07:11 +0100

    Initial

--- app/src/main/java/com/example/tp2partie3/MainActivity.java
+++ app/src/main/java/com/example/tp2partie3/MainActivity.java
@@ -9,13 +9,9 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button send = findViewById(R.id.sendbtn);
+        Button send = findViewById(R.id.sendeventbtn);
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -52,9 +48,9 @@ public class MainActivity extends AppCompatActivity {
 
         filter.addAction(Intent.ACTION_BATTERY_LOW);
 
-        textViewBattery = findViewById(R.id.textViewBattery);
+        textViewBattery = findViewById(R.id.viewBattery);
 
-        textViewReceiver = findViewById(R.id.textViewReceiver);
+        textViewReceiver = findViewById(R.id.viewReceiver);
 
 
 
